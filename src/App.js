ae69aab5a1efac6c8735ed39e7a3a5feda4c6d11
David Winalda <davidwinalda94@gmail.com>
2021-11-07 23:30:15 +0700

    add tech assignment react test with jest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import { AddTodo } from './components/helper';
+
+const App = () => {
+  const defaultTodo = [
+    {
+      id: 1,
+      name: 'Learn JSX',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      name: 'Build awasome react app',
+      isComplete: false,
+    },
+    {
+      id: 3,
+      name: 'Ship it',
+      isComplete: false,
+    }
+  ];
+  const [currentToDo, setCurrentToDo] = useState('');
+  const [todos, setTodos] = useState(defaultTodo);
+
+  const handleOnChangeInput = (e) => {
+    e.preventDefault();
+    setCurrentToDo(e.target.value)
+  }
+
+  const generateId = () => Math.floor(Math.random() * 100000)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: generateId(),
+      name: currentToDo,
+      isComplete: false
+    }
+
+    const updateTodos = AddTodo(todos, newTodo);
+    setTodos(updateTodos);
+    setCurrentToDo('');
+  }
+
+  console.log(currentToDo)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Todolist App</h1>
+
+      <TodoForm handleSubmit={handleSubmit} handleOnChangeInput={handleOnChangeInput} currentTodo={currentToDo} />
+      <TodoList todos={todos} />
     </div>
   );
 }
